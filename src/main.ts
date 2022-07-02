bad75620711c3fd4261eace13ab0a78debd0c0cf
Maisnam Raju Singh <maisnamraju@gmail.com>
2022-07-02 22:09:57 +0700

    first push

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API for POC')
+    .setDescription('API for POC')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(PORT);
+}
+bootstrap();
